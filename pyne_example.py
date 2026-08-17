94a47b726af509d931e03bbf170f727a7dcd9a45
Ordnmash <ordnfxist@gmail.com>
2026-08-17 21:08:45 +0200

    Fix sum operation to specify tensor element

--- pyne_example.py
+++ pyne_example.py
@@ -4,5 +4,5 @@ c = a + b
 d = c * pyne.tensor(4.0)
 e = pyne.tensor([[2,4], [4,2], [3,1], [4,2]])   # shape pyne.Size([4,2])
 f = d @ e                                       # output shape = pyne.Size([2,2]) after matrix multiplying
-g = f.sum()                                     # sum to a 1d tensor
+g = f.sum()                                     # sum to a 1d tensor of element of 1
 g.backward()                                    # computes dG/dF, dG/dE, dG/dD, dG/dC, dG/dB, dG/dA
